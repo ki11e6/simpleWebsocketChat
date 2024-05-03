2208d3518cd29b20d48fb195b5e0535c595cf4d1
ki11e6 <sharath.surendran.m@gmail.com>
2024-05-03 13:00:00 +0530

    socket.io

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,24 @@
-import { WebSocketServer } from 'ws';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
 
-const server = new WebSocketServer({ port: '3000' });
+const httpServer = createServer();
 
-server.on('connection', (socket) => {
-  socket.on('message', (message) => {
-    //converting message buffer to string
-    console.log(Buffer.from(message).toString());
-    socket.send(`${message}`);
+const io = new Server(httpServer, {
+  cors: {
+    origin:
+      process.env.NODE_ENV === 'production'
+        ? false
+        : ['http://localhost:5500', 'http://127.0.0.1:5500'],
+  },
+});
+
+io.on('connection', (socket) => {
+  console.log(`User ${socket.id} connected`);
+
+  socket.on('message', (data) => {
+    console.log(data);
+    io.emit('message', `${socket.id.substring(0, 5)}: ${data}`);
   });
 });
+
+httpServer.listen(3500, () => console.log('listening on port 3500'));
--- frontend/app.js
+++ frontend/app.js
@@ -1,11 +1,10 @@
-const socket = new WebSocket('ws://localhost:3000');
-//sent message
+const socket = io('ws://localhost:3500');
+
 function sendMessage(e) {
   e.preventDefault();
-  console.log('working');
   const input = document.querySelector('input');
   if (input.value) {
-    socket.send(input.value);
+    socket.emit('message', input.value);
     input.value = '';
   }
   input.focus();
@@ -13,8 +12,8 @@ function sendMessage(e) {
 
 document.querySelector('form').addEventListener('submit', sendMessage);
 
-//listen message
-socket.addEventListener('message', ({ data }) => {
+// Listen for messages
+socket.on('message', (data) => {
   const li = document.createElement('li');
   li.textContent = data;
   document.querySelector('ul').appendChild(li);
